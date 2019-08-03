bc7efe650fc0ba69930fb8b202025f9a08504dc0
OrangeM21 <orangemtony@gmail.com>
2019-08-04 05:30:59 +0800

    fix

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,7 +3,7 @@
     <el-header>
       <el-row type="flex" :align="middle">
         <el-col :span="10" :offset="2">
-          <img src="../assets/Desktop.png" alt class="header-image" />
+          <img src="../assets/serios-logo.svg" alt class="header-image" />
         </el-col>
         <el-col :span="10">
           <el-menu class="topHeader" mode="horizontal">
@@ -19,8 +19,30 @@
         </el-col>
       </el-row>
     </el-header>
-    <el-main>
-      
+    <el-main class="main">
+      <el-row type="flex" :align="middle">
+        <el-col :span="7">
+          <img src="../assets/main-left.svg" >
+        </el-col>
+        <el-col :span="10">
+          <h1>The standard for secure blockchain applications</h1>
+          <p style="text-align: justify;">Serios provides tools to write, deploy and operate decentralized applications. We also protect leading organizations by performing security audits on their systems and products.</p>
+        </el-col>
+        <el-col :span="7">
+          <img src="../assets/main-right.svg" >
+        </el-col>
+      </el-row>
+      <el-row class="main_2">
+        <el-col :offset="6" :span="4">
+          <img src="../assets/nervos-logo.svg">
+        </el-col>
+        <el-col :span="4">
+          <img src="../assets/Rust.svg">
+        </el-col>
+        <el-col :span="4">
+          <img src="../assets/Community.svg">
+        </el-col>
+      </el-row>
     </el-main>
     <el-footer>Footer</el-footer>
   </el-container>
@@ -33,6 +55,9 @@ export default {
 </script>
 
 <style scoped>
+.main{
+  margin-top:5rem;
+}
 .el-menu.el-menu--horizontal {
   border-bottom: 0px;
 }
@@ -42,5 +67,8 @@ export default {
 .header-image {
   float: left;
   margin-top: 0.45rem;
+}
+.main_2{
+  margin-top:5rem;
 }
 </style>
